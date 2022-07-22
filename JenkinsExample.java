9c5d8ed787270c42409f013c3dbba72aa5fe2e3e
arumugaraja2006 <32137170+arumugaraja2006@users.noreply.github.com>
2022-07-22 15:40:45 -0400

    Update JenkinsExample.java

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -4,5 +4,6 @@ public static void main(String[] args){
 System.out.println("Hello Jenkins");
 System.out.println("Hello cicd");
   System.out.println("Hello github");
+   System.out.println("Hello github");
 }
 }
